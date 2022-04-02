cbbf30c80ace4e7875c1813685c72de8e879c7d7
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-02 20:23:21 +0300

    some small fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import { setGroup, setWort, setSelected } from './reducers/mainReducer'
 import MainWortPage from './components/mainWortPage/MainWortPage'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Provider, useDispatch } from 'react-redux'
 import * as ReactDOMClient from 'react-dom/client'
+import { Provider } from 'react-redux'
 import store from './store'
 import React from 'react'
-import axios from 'axios'
 
 
 function App() {
-    // const dispatch = useDispatch()
-    // React.useEffect(_ => {
-    //     axios.get("/api/group/")
-    //         .then(d => {
-    //             const g = d.data
-    //             dispatch(setGroup({ group: g }))
-    //         })
-    //         .catch(e => console.log(e))
-    //     axios.get("/api/wort/")
-    //         .then(w => {
-    //             dispatch(setWort({ wort: w.data }))
-    //         })
-    //         .catch(e => console.log(e))
-    // }, [])
-
     return (
         <div>
             <BrowserRouter>
