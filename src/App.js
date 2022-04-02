7c4a97846c6fd64a19a23b5a0e156819aa00f33f
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-02 20:21:50 +0300

    add router to App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import { setGroup, setWort, setSelected } from './reducers/mainReducer'
+import MainWortPage from './components/mainWortPage/MainWortPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Provider, useDispatch } from 'react-redux'
 import * as ReactDOMClient from 'react-dom/client'
-import Wort from './components/wort'
 import store from './store'
 import React from 'react'
 import axios from 'axios'
-import Group from './components/group'
 
 
 function App() {
-    const dispatch = useDispatch()
-    React.useEffect(_ => {
-        axios.get("/api/group/")
-            .then(d => {
-                const g = d.data
-                dispatch(setGroup({ group: g }))
-            })
-            .catch(e => console.log(e))
-        axios.get("/api/wort/")
-            .then(w => {
-                dispatch(setWort({ wort: w.data }))
-            })
-            .catch(e => console.log(e))
-    }, [])
+    // const dispatch = useDispatch()
+    // React.useEffect(_ => {
+    //     axios.get("/api/group/")
+    //         .then(d => {
+    //             const g = d.data
+    //             dispatch(setGroup({ group: g }))
+    //         })
+    //         .catch(e => console.log(e))
+    //     axios.get("/api/wort/")
+    //         .then(w => {
+    //             dispatch(setWort({ wort: w.data }))
+    //         })
+    //         .catch(e => console.log(e))
+    // }, [])
 
     return (
         <div>
-            <Group />
-            <Wort />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainWortPage />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
