da03c827c8aa27bd71d7232fb4b1d6634984b62e
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-03 11:00:29 +0300

    add error404 file

---
+++ src/components/Error404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error404() {
+    return (
+        <div style={{ width: "100%", textAlign: "center", marginTop: "7rem", fontWeight: "bolder", fontSize: "36px" }}>
+            Error404
+        </div>
+    )
+}
+
+export default Error404
