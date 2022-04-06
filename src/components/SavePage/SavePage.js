ce9633697836da5f1c809108ae6347955af071ad
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-06 22:13:42 +0300

    delete separate axios request from save page

--- src/components/SavePage/SavePage.js
+++ src/components/SavePage/SavePage.js
@@ -1,42 +1,13 @@
 import { WortList, WortDescription, ThemeList } from './components'
-import { setState, initialWort } from '../../reducers/mainReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import WortInput from './components/WortInput'
 import { NavLink } from "react-router-dom"
+import { useSelector } from 'react-redux'
 import className from 'classnames'
-import axios from 'axios'
 import React from 'react'
 
 function SavePage() {
     const loading = useSelector(state => state.main.loading)
     const group = useSelector(state => state.main.group)
-    const dispatch = useDispatch()
-
-    React.useEffect(_ => {
-        dispatch(setState({ loading: true }))
-        Promise.all(([
-            axios.get("/api/worttheme/"),
-            axios.get("/api/group/"),
-            axios.get("/api/theme/"),
-            axios.get("/api/wort/"),
-        ]))
-            .then(values => {
-                const [wortTheme, group, theme, wort] = values
-                const currentWort = wort.data?.[0] || initialWort
-                dispatch(setState({
-                    wortTheme: wortTheme.data,
-                    currentWort: currentWort,
-                    group: group.data,
-                    theme: theme.data,
-                    wort: wort.data,
-                    loading: false,
-                }))
-            })
-            .catch(e => {
-                dispatch(setState({ loading: false }))
-                console.log(e)
-            })
-    }, [])
 
     if (loading) return <h1>Loading...</h1>
     else if (group.length == 0) return (
