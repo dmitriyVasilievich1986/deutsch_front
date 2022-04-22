973a92fa97126ed333e3149fc433cfc4bd20db6f
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-23 00:16:00 +0300

    change wort description and list components

--- src/components/SavePage/components/WortDescription.js
+++ src/components/SavePage/components/WortDescription.js
@@ -1,12 +1,37 @@
 import { setState } from '../../../reducers/mainReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import className from 'classnames'
+import Select from '../../Select'
 import axios from 'axios'
 import React from 'react'
+
+function Delete(props) {
+    let timer = React.useRef(null)
+
+    const downHandler = _ => {
+        timer.current = setTimeout(_ => props.deleteHandler(), 2400)
+    }
+
+    const upHandler = _ => {
+        timer.current && clearTimeout(timer.current)
+    }
+
+    return (
+        <div className={className("test_class")}>
+            <img
+                className={className("icon", "mt2")}
+                onMouseDown={downHandler}
+                src="/static/i/bin.png"
+                onMouseUp={upHandler}
+            />
+        </div>
+    )
+}
 
 function WortDescription() {
     const dispatch = useDispatch()
     const currentWort = useSelector(state => state.main.currentWort)
+    const wortTheme = useSelector(state => state.main.wortTheme)
     const group = useSelector(state => state.main.group)
     const wort = useSelector(state => state.main.wort)
 
@@ -43,6 +68,21 @@ function WortDescription() {
         setGroupID(g)
     }
 
+    const deleteHandler = _ => {
+        const currentID = currentWort.id
+        axios.delete(`/api/wort/${currentID}/`)
+            .then(_ => {
+                dispatch(setState({
+                    wortTheme: wortTheme.filter(wt => wt.wort != currentID),
+                    wort: wort.filter(w => w.id != currentID),
+                    currentWort: wort[0],
+                }))
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
     if (currentWort.id == 0) return null
     return (
         <div className={className("m2")}>
@@ -59,24 +99,23 @@ function WortDescription() {
                     <div>Translate:</div>
                     <div><input className={className("input")} type="text" value={translate} onChange={e => setTranslate(e.target.value)} /></div>
                 </div>
-            </div>
-            <div className={className("long_list")}>
-                {group.map(g => (
-                    <div
-                        className={className("wort_group", {
-                            selected: g.id == currentWort.group,
-                            active: g.id == groupID,
-                        })}
-                        onClick={_ => setGroupID(g.id)}
-                        key={g.id}
-                    >
-                        {g.name}
+                <div className={className("description_field")}>
+                    <div>Group:</div>
+                    <div>
+                        <Select
+                            value={group.filter(g => g.id == currentWort.group)[0]}
+                            changeHandler={newGroup => setGroupID(newGroup.id)}
+                            groupList={group}
+                        />
                     </div>
-                )
-                )}
+                </div>
             </div>
-            <img src="/static/i/eraser.png" onClick={eraserHandler} className={className("icon", "mt2")} />
-            <img src="/static/i/save.png" onClick={_ => saveWort()} className={className("icon", "mt2")} />
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <img src="/static/i/eraser.png" onClick={eraserHandler} className={className("icon", "mt2")} />
+                <img src="/static/i/save.png" onClick={_ => saveWort()} className={className("icon", "mt2")} />
+                <Delete deleteHandler={deleteHandler} />
+            </div>
+
         </div>
     )
 }
--- src/components/SavePage/components/WortList.js
+++ src/components/SavePage/components/WortList.js
@@ -1,83 +1,21 @@
-import { setState, initialWort } from '../../../reducers/mainReducer'
+import { setState } from '../../../reducers/mainReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import className from 'classnames'
+import Select from '../../Select'
 import React from 'react'
-import axios from 'axios'
-
 
 function WortList() {
     const currentWort = useSelector(state => state.main.currentWort)
-    const wortTheme = useSelector(state => state.main.wortTheme)
     const wort = useSelector(state => state.main.wort)
-
-    const [wortList, setWortList] = React.useState(wort)
-    const [search, setSearch] = React.useState("")
-    const listRef = React.useRef(null)
     const dispatch = useDispatch()
 
-    React.useEffect(_ => {
-        if (listRef.current) {
-            listRef.current.getElementsByClassName("active")?.[0]?.scrollIntoView({ behavior: "smooth" })
-        }
-    }, [listRef])
-    React.useEffect(_ => {
-        const e = wortList.filter(l => l.id == currentWort.id)
-        if (e.length == 0 && wortList.length > 0) {
-            dispatch(setState({ currentWort: wortList[0] }))
-        }
-    }, [wortList])
-
-    React.useEffect(_ => {
-        if (search == "" || search == " ") {
-            setWortList(wort)
-        } else {
-            const l = wort.filter(w => w.wort.toLowerCase().includes(search.toLowerCase()))
-            setWortList(l)
-        }
-    }, [search])
-
-    const deleteHandler = id => {
-        axios.delete(`/api/wort/${id}/`)
-            .then(_ => {
-                const w = wort.filter(w => w.id != id)
-                const c = w?.[0] || initialWort
-                const wtList = wortTheme.filter(wt => wt.wort != id)
-                dispatch(setState({
-                    wortTheme: wtList,
-                    currentWort: c,
-                    wort: w,
-                }))
-            })
-            .catch(e => {
-                console.log(e)
-            })
-    }
-
-    if (wort.length === 0 || currentWort === null) return null
     return (
-        <div className={className('wort_list_wrapper')}>
-            <div className={className("wort_list_inner")}>
-                <div className={className('input_row', 'mb2', 'ml2')}>
-                    <img src="/static/i/search.png" className='search_icon' />
-                    <input type="text" value={search} onChange={e => setSearch(e.target.value)} className="search" />
-                    <img src="/static/i/eraser.png" onClick={_ => setSearch("")} className="icon" />
-                </div>
-                <div className={className('wort_list')}>
-                    <div ref={listRef}>
-                        {wortList.map(w => (
-                            <div className={className("input_row")} key={w.id}>
-                                <div
-                                    className={className("wort", { active: w.id == currentWort.id })}
-                                    onClick={_ => dispatch(setState({ currentWort: w }))}
-                                >
-                                    {w.wort}<div className={className("ml2", "mr2")}>/</div>{w.translate}
-                                </div>
-                                <img src="/static/i/bin.png" onClick={_ => deleteHandler(w.id)} className="icon" />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+        <div style={{ margin: "2rem", width: "80%" }}>
+            <Select
+                value={{ name: `${currentWort.wort} / ${currentWort.translate}`, ...currentWort }}
+                groupList={wort.map(w => ({ name: `${w.wort} / ${w.translate}`, ...w }))}
+                changeHandler={newWort => dispatch(setState({ currentWort: newWort }))}
+            />
         </div>
     )
 }
