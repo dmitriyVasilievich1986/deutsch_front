e689148600e7f86c05e060c9cfe29c802ed9f800
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-08-03 08:34:03 +0200

    delete group route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Navbar(props) {
     const routes = [
         { path: "/", name: "Main" },
-        { path: "/group/", name: "Groups" },
+        // { path: "/group/", name: "Groups" },
         { path: "/theme/", name: "Themes" },
         { path: "/save/", name: "Words" },
     ];
